889a91f5fbc5bceae989992a1d1ab3516d237e9f
MahmoudAdelI <mahmoud.adeli7888@gmail.com>
2024-11-21 18:11:08 +0200

    starter

--- src/app/lib/fetchBlogs.ts
+++ src/app/lib/fetchBlogs.ts
@@ -1,8 +1,7 @@
-import next from "next";
 import type { blogs } from "../types";
 export default async function fetchBlogs(url:string):Promise<blogs | undefined> {
 
-    const res = await fetch('https://amused-stripe-play.glitch.me/blogs', {cache: 'no-store'});
+    const res = await fetch(url, {cache: 'no-store'});
     const blogs:blogs = await res.json();
 
   return blogs
